47e74308aab2ec8a8a95790e62a5e6894c767945
Nico Rithner <nicorithner@gmail.com>
2023-05-24 13:18:48 -0600

    Create Get endpoint validateUsername

--- src/main/java/com/cooksys/social_media_api/controllers/ValidateController.java
+++ src/main/java/com/cooksys/social_media_api/controllers/ValidateController.java
@@ -1,18 +1,22 @@
 package com.cooksys.social_media_api.controllers;
 
+import com.cooksys.social_media_api.services.ValidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.social_media_api.services.ValidateService;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/validate")
 public class ValidateController {
+    private final ValidateService validateService;
 
-   
+    @GetMapping("/username/exists/@{username}")
+
+    public Boolean validateUsername(@PathVariable String username) {
+        return validateService.validateUsername(username);
+    }
 
 }
