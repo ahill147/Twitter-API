47485cddc7e15f3e840b3fe40f06ea7679e929d6
Nico Rithner <nicorithner@gmail.com>
2023-05-24 19:36:03 -0600

    Add application.properties

--- src/test/java/com/cooksys/social_media_api/SocialmediaApiApplicationTests.java
+++ src/test/java/com/cooksys/social_media_api/SocialmediaApiApplicationTests.java
@@ -1,11 +1,8 @@
 package com.cooksys.social_media_api;
 
-import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class SocialmediaApiApplicationTests {
 
